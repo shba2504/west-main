a97df7be5581de8c80e481286ed3869d030ae3c1
Sheena Marshalla Barnes <shba2504@gmail.com>
2024-04-05 23:42:24 -0600

    mobile navbar styling

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ openModal.addEventListener("click", () => {
 closeModal.addEventListener("click", () => {
   modal.close();
 });
+
+// NAVBAR //
+
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+
+navToggle.addEventListener("click", () => {
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
+  primaryNav.toggleAttribute("data-visible");
+});
